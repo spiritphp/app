6d54a19b2f30562834f0e1f687931265ee27660d
Marat Nuriev <nurieff@gmail.com>
2017-09-19 00:44:18 +0500

    Middleware in RoutePlugin

--- app/Plugins/RoutePlugin.php
+++ app/Plugins/RoutePlugin.php
@@ -4,8 +4,20 @@ namespace App\Plugins;
 
 use Spirit\Route;
 use Spirit\Route\RoutePlugin as Plugin;
+use Spirit\Route\Middleware as VendorMiddleware;
 
 class RoutePlugin extends Plugin {
+
+    protected function middleware(Route\Routing $routing)
+    {
+        $routing->addMiddleware([
+            'auth' => VendorMiddleware\Auth::class,
+            'guest' => VendorMiddleware\Guest::class,
+            'role' => VendorMiddleware\Role::class,
+            'throttle' => VendorMiddleware\Throttle::class,
+            'token' => VendorMiddleware\Token::class,
+        ]);
+    }
 
     protected function routes(Route\Routing $routing)
     {
@@ -13,6 +25,12 @@ class RoutePlugin extends Plugin {
             'namespace' => 'App\Controllers'
         ], function(){
             $this->loadRoute('web.php');
+        });
+
+        $routing->group([
+            'prefix' => 'auth'
+        ], function(){
+            $this->loadRoute('auth.php');
         });
     }
 }
--- routes/auth.php
+++ routes/auth.php
@@ -1,40 +1,33 @@
 <?php
 
-Route::group([
-    'prefix' => 'auth'
-],function(){
+Route::get('captcha/{unique_id}','AuthController@captcha');
 
-    Route::get('captcha/{unique_id}','AuthController@captcha');
+Route::add('login',[
+    'uses' => 'AuthController@login',
+    'middleware' => ['guest']
+]);
 
-    Route::add('login',[
-        'uses' => 'AuthController@login',
-        'middleware' => ['guest']
-    ]);
+Route::add('registration',[
+    'uses' => 'AuthController@registration',
+    'middleware' => ['guest']
+]);
 
-    Route::add('registration',[
-        'uses' => 'AuthController@registration',
-        'middleware' => ['guest']
-    ]);
+Route::add('logout',[
+    'uses' => 'AuthController@logout',
+    'middleware' => ['auth']
+]);
 
-    Route::add('logout',[
-        'uses' => 'AuthController@logout',
-        'middleware' => ['auth']
-    ]);
+Route::add('recovery/{hash?}',[
+    'uses' => 'AuthController@recovery',
+    'middleware' => ['guest']
+]);
 
-    Route::add('recovery/{hash?}',[
-        'uses' => 'AuthController@recovery',
-        'middleware' => ['guest']
-    ]);
+Route::add('app/{type?}',[
+    'uses' => 'AuthController@app',
+    'middleware' => ['guest']
+]);
 
-    Route::add('app/{type?}',[
-        'uses' => 'AuthController@app',
-        'middleware' => ['guest']
-    ]);
-
-    Route::add('activation/{code}',[
-        'uses' => 'AuthController@activation',
-        'middleware' => ['guest']
-    ]);
-
-});
-
+Route::add('activation/{code}',[
+    'uses' => 'AuthController@activation',
+    'middleware' => ['guest']
+]);
