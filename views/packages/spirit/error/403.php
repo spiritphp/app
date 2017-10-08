ce6287743fda982b775f2d84902c3fa1d52cb7d0
Marat Nuriev <nurieff@gmail.com>
2017-10-09 00:25:08 +0500

    errors

---
+++ views/packages/spirit/error/403.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100,300,700,400&subset=latin,cyrillic' rel='stylesheet'
+          type='text/css'>
+
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #696969;
+            display: table;
+            font-weight: 100;
+            font-family: 'Roboto';
+            line-height: 1;
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 20px;
+            margin-bottom: 20px;
+        }
+
+        @media only screen and (min-width: 640px) {
+            .title {
+                font-size: 40px;
+            }
+        }
+
+        .title__link {
+            color: #0047FF;
+            text-decoration: none;
+            border-bottom: 2px solid #CFDCFF;
+        }
+
+        .title__link:hover {
+            color: #FF0000;
+            text-decoration: none;
+            border-bottom: 2px solid #FF6969;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="title">403 | Доступ запрещён</div>
+        <div class="title">&larr; <a href="/" class="title__link">Вернуться на главную</a></div>
+
+    </div>
+</div>
+</body>
+</html>
