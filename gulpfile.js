f88cca25261aaf62d69e690cc07e7a009562ce96
Marat Nuriev <nurieff@gmail.com>
2017-10-15 01:14:58 +0500

    gulp-query-copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 let builder = require('gulp-query')
   , webpack = require('gulp-query-webpack')
   , scss = require('gulp-query-scss')
+  , copy = require('gulp-query-copy')
 ;
 
 builder((query) => {
   query
-    .plugins(webpack, scss)
+    .plugins(webpack, scss, copy)
     .webpack('resources/assets/js/app.js','public/js','app')
     .scss('resources/assets/scss/app.scss','public/css','app')
+
+    //.copy('node_modules/jquery/dist/jquery.min.js','public/js','jquery')
   ;
 });
